54c22faf8e8a8f26f03ba3375583e2cd5a869769
Егор Тупикин <egor_tupikin02@mail.ru>
2023-01-25 01:55:14 +0300

    front

--- store/product/views.py
+++ store/product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {'title': 'Test Title',
+               'username': 'Valeria'
+    }
+    return render(request, 'product/index.html',context)
+
+def products(request):
+    return render(request, 'product/products.html')
--- store/store/settings.py
+++ store/store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+2$ecj!5szo8uj^u23+2s_n4g5ps(y4mh&l_-@b0ze=f_e9gio
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
