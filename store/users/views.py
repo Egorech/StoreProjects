c0009f95f13fadb0dccc5196db5018413b94b2e3
Егор Тупикин <egor_tupikin02@mail.ru>
2023-01-27 12:09:52 +0300

    user_form

--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from  users.models import User
+from  users.forms import UserLoginForm
 # Create your views here.
 
 def login(request):
-    return render(request, 'users/login.html')
+    context = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', context)
 
 def register(request):
     return render(request, 'users/register.html')
