3496e4cfd362a5c6cc2ea2d97cd02d52955b45e5
Егор Тупикин <egor_tupikin02@mail.ru>
2023-01-27 17:56:47 +0300

    user_conf

--- store/users/views.py
+++ store/users/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
 from  users.models import User
-from  users.forms import UserLoginForm
+from  users.forms import UserLoginForm, UserRegistrationForm
 # Create your views here.
+from django.contrib import auth
 
 def login(request):
-    context = {'form': UserLoginForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
     return render(request, 'users/login.html', context)
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data = request.POST)
+        if form.is_valid():
+            print(123)
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
