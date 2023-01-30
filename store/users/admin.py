54b3345e1b1a5fe3638720ab5969862f6eb78c11
Егор Тупикин <egor_tupikin02@mail.ru>
2023-01-30 18:41:31 +0300

    email

--- store/users/admin.py
+++ store/users/admin.py
@@ -4,3 +4,9 @@ from .models import *
 # Register your models here.
 
 admin.site.register(User)
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'user', 'expiration')
+    fields = ('code', 'user', 'expiration', 'created')
+    readonly_fields = ('created',)
